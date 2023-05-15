1069b5e0c1eb9abdf1960fb31ce6ffdf1846863e
grencez <git@grencez.dev>
2023-05-14 23:14:57 -0700

    build(dep): Update llama.cpp for new ggml format
    Fixes #26

--- dep/cmake_fetchcontent/llama_cpp.cmake
+++ dep/cmake_fetchcontent/llama_cpp.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
   LlamaCpp
   GIT_REPOSITORY "https://github.com/ggerganov/llama.cpp.git"
-  GIT_TAG "1b0fd454650ef4d68a980e3225488b79e6e9af25"
+  GIT_TAG "63d20469b85467c5729cc9a97bd44cc3da63423f"
 )
 FetchContent_MakeAvailable(LlamaCpp)
 set(LlamaCpp_INCLUDE_DIRS "${llamacpp_SOURCE_DIR}" PARENT_SCOPE)
