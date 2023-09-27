a1ef449d0a761303a495d4e53a1634bdfe7535f8
grencez <git@grencez.dev>
2023-09-26 23:35:51 -0700

    update(llama.cpp): with new GGUF file format
    Fixes #34

--- test/manual/chat.sh
+++ test/manual/chat.sh
@@ -2,10 +2,10 @@
 set -e -u
 
 # Try running from the project's toplevel directory as:
-#  ./test/manual/chat.sh roshambo_kira -- --model ../llama.cpp/models/7B/ggml-model-q4_0.bin
+#  ./test/manual/chat.sh roshambo_kira -- --model ../llama.cpp/models/7B/ggml-model-q4_0.gguf
 
 # These will be overridden by given args.
-model_file="../llama.cpp/models/7B/ggml-model-q4_0.bin"
+model_file="../llama.cpp/models/7B/ggml-model-q4_0.gguf"
 thread_count="8"
 
 # First arg must be the test prompt name.
