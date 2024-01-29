efe3391ecb2ed95d3e073d8cc153f6edddbe4d9a
grencez <git@grencez.dev>
2024-01-29 15:01:19 -0800

    update(fildesh): with Sxpb manipulation functions

--- dep/cmake_fetchcontent/fildesh.cmake
+++ dep/cmake_fetchcontent/fildesh.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
   Fildesh
   GIT_REPOSITORY "https://github.com/fildesh/fildesh.git"
-  GIT_TAG "ec241c6b0e895bc1869f08a2f8a2b268ce9420c1"
+  GIT_TAG "3d64380eff85034d6bf531f816453761f40e59fd"
 )
 FetchContent_MakeAvailable(Fildesh)
 set(Fildesh_INCLUDE_DIRS ${Fildesh_INCLUDE_DIRS} PARENT_SCOPE)
