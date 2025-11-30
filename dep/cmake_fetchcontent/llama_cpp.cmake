03d36397d8374e339f5d95e3bfdf45f58c357342
grencez <git@grencez.dev>
2025-11-29 21:36:32 -0800

    qual(cmake): support Vulcan build

--- dep/cmake_fetchcontent/llama_cpp.cmake
+++ dep/cmake_fetchcontent/llama_cpp.cmake
@@ -4,16 +4,8 @@ FetchContent_Declare(
   GIT_TAG "d9c6ce46f747189cd6238ca7699253613f77c016"
 )
 
-set(GGML_OPENMP FALSE CACHE BOOL "OpenMP off for compatibility.")
 FetchContent_MakeAvailable(LlamaCpp)
 
 set(LlamaCpp_SOURCE_DIR "${llamacpp_SOURCE_DIR}" PARENT_SCOPE)
 set(LlamaCpp_INCLUDE_DIRS "${llamacpp_SOURCE_DIR}/include" PARENT_SCOPE)
 set(LlamaCpp_LIBRARIES "$<TARGET_NAME:llama>" PARENT_SCOPE)
-
-if(LLAMA_OPENBLAS_ON)
-  find_package(OpenBLAS REQUIRED)
-  target_compile_definitions(ggml PRIVATE "GGML_USE_OPENBLAS")
-  target_include_directories(ggml PRIVATE ${OpenBLAS_INCLUDE_DIRS})
-  target_link_libraries(ggml PUBLIC ${OpenBLAS_LIBRARIES})
-endif()
